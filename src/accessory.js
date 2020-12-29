05e7908174c16c11bf7a9b212d512b6728de67f4
meg768 <meg768@users.noreply.github.com>
2020-12-29 17:51:05 +0100

    -

--- src/accessory.js
+++ src/accessory.js
@@ -1,7 +1,6 @@
 var {API, Service, Characteristic} = require('./homebridge.js');
 
-
-class Accessory extends API.hap.Accessory  {
+module.exports = class extends API.platformAccessory  {
 
     constructor(options) {
         var {log, debug, config, name, platform, uuid} = options;
@@ -18,12 +17,10 @@ class Accessory extends API.hap.Accessory  {
         if (uuid == undefined)
             uuid = API.hap.uuid.generate(name);
 
-		console.log('CREATING ACCESSORY', name, uuid);
 		super(name, uuid);
 
 		this.name = name;
-//        this.displayName = name;
-
+        this.displayName = name;
 		this.platform = platform;
 		this.config = config;
 		this.log = log;
@@ -31,14 +28,12 @@ class Accessory extends API.hap.Accessory  {
 
 		this.updateCharacteristicValue(Service.AccessoryInformation, Characteristic.FirmwareRevision, "1.0");
 
-		console.log(this);
-  
   }
-/*
+
 	getServices() {
 		return this.services;
 	}
-*/	
+	
 	pause(ms) {
         return new Promise((resolve, reject) => {
             setTimeout(resolve, ms);
@@ -83,109 +78,5 @@ class Accessory extends API.hap.Accessory  {
 
 
 };
-
-/*
-class Accessory extends Events {
-
-    constructor(options) {
-        super();
-
-        var {log, debug, config, name, platform, uuid} = options;
-
-		if (config == undefined)
-			throw new Error('A configuration of the accessory must be specified.');
-
-		if (name == undefined)
-			name = config.name;
-
-        if (name == undefined)
-            throw new Error('A name of the accessory must be specified.');
-
-        if (uuid == undefined)
-            uuid = homebridge.hap.uuid.generate(name);
-
-        this.services = [];
-
-        var service = new Service.AccessoryInformation();
-        service.getCharacteristic(Characteristic.FirmwareRevision, "1.0");
-        this.addService(service); 
-
-        // Seems like we have to give it a name...
-        this.name = name;
-        this.displayName = name;
-		this.UUID = uuid;
-		this.platform = platform;
-		this.config = config;
-		this.log = log;
-		this.debug = debug;
-    }
-
-    pause(ms) {
-        return new Promise((resolve, reject) => {
-            setTimeout(resolve, ms);
-        });
-    }
-    
-    addService(service) {
-        this.services.push(service);
-    }
-
-    updateCharacteristicValue(service, characteristic, value) {
-        this.getService(service).getCharacteristic(characteristic).updateValue(value);
-    }
-
-    enableCharacteristic(service, characteristic, getter, setter) {
-
-        service = this.getService(service);
-        
-        if (typeof getter === 'function') {
-            service.getCharacteristic(characteristic).on('get', callback => {
-                callback(null, getter());
-            });
-        }
-
-        if (typeof setter === 'function') {
-            service.getCharacteristic(characteristic).on('set', (value, callback) => {
-                var response = setter(value);
-
-                if (response instanceof Promise) {
-                    response.then(() => {
-                    })
-                    .catch((error) => {
-                        this.log(error);
-                    })
-                    .then(() => {
-                        callback(null, getter());                
-                    });
-                }
-                else
-                    callback(null, getter());
-            });
-    
-        }
-
-    }
-
-    getService(name) {
-        if (name instanceof Service)
-            return name;
-
-        for (var index in this.services) {
-            var service = this.services[index];
-            
-            if (typeof name === 'string' && (service.displayName === name || service.name === name))
-                return service;
-            else if (typeof name === 'function' && ((service instanceof name) || (name.UUID === service.UUID)))
-                return service;
-          }
-        
-    }
-
-    getServices() {
-        return this.services;
-    }
-};
-
-*/
 
 module.exports = Accessory;
