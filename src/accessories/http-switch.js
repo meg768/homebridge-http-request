3916e58f72046c8d14cfbb51eab7612bddaca902
meg768 <meg768@users.noreply.github.com>
2020-12-31 16:51:21 +0100

    -

--- src/accessories/http-switch.js
+++ src/accessories/http-switch.js
@@ -1,53 +1,65 @@
+var {Service, Characteristic} = require('../homebridge.js')
+var Accessory = require('../accessory.js');
 
-var Switch = require('./switch.js');
-
-module.exports = class extends Switch {
+module.exports = class extends Accessory {
 
     constructor(options) {
 
-        super(options);
-    }
-
-    turnOn() {
-		var Request = require('yow/request');
-		var isObject = require('yow/isObject');
+		super(options);
 		
-		if (!isObject(this.config.request))
-			return Promise.resolve();
+		var state = false;
+		
+		var setter = (value) => {
+			var Request = require('yow/request');
+			var isObject = require('yow/isObject');
 
-		return new Promise((resolve, reject) => {
-			var {method = 'get', url, query, body} = this.config.request;
+			if (!isObject(this.config.request))
+				return Promise.resolve();
 
-			this.debug('Connecting to', url, 'using method', method, '...');
-			var request = new Request(url);
-			var options = {};
+			var characteristic = this.getService(Service.Switch).getCharacteristic(this.Characteristic.On);
+
+			return new Promise((resolve, reject) => {
+				var {method = 'get', url, query, body} = this.config.request;
 	
-			if (isObject(body))
-				options.body = body;
+				this.debug('Connecting to', url, 'using method', method, '...');
+				var request = new Request(url);
+				var options = {};
+		
+				if (isObject(body))
+					options.body = body;
+		
+				if (isObject(query))
+					options.query = query;
+		
+				request.request(method, options).then(() => {
+					console.log('request send pausing 1000 ms');
+					return this.pause(1000);
+				})
+				.catch((error) => {
+					this.log(error);
+				})
+				.then(() => {
+					console.log('Turning off again...');
+					characteristic.updateValue(state = false);
+				})
+				.then(() => {
+					resolve();
+				})
+				.catch((error) => {
+					reject(error);
+				})
 	
-			if (isObject(query))
-				options.query = query;
-	
-			request.request(method, options).then(() => {
-				console.log('request send pausing 1000 ms');
-				return this.pause(1000);
-			})
-			.then(() => {
-				console.log('Turning off again...');
-				return this.updateSwitchState(false);
-			})
-			.then(() => {
-				resolve();
-			})
-			.catch((error) => {
-				reject(error);
-			})
+			});
+				
+		};
 
-		});
-    }
+		var getter = () => {
+			return state;
+		};
 
-    turnOff() {
-        return Promise.resolve();
+        this.addService(new Service.Switch(this.name, this.UUID));
+        this.enableCharacteristic(Service.Switch, Characteristic.On, getter, setter);
+
     }
 
 }
